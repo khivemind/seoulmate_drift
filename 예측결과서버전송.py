00085e28f5a8d50ba1b209d9f222819541a9dfa6
syban <syban75@naver.com>
2026-05-19 10:10:15 +0900

    pem

---
+++ 예측결과서버전송.py
@@ -0,0 +1,38 @@
+import paramiko
+
+# 접속 정보
+hostname = "34.81.221.132"
+username = "overlord"
+pem_file = "overlord.pem"
+
+local_file = f"D:\seoulmate\도시활력지수.csv"
+remote_file = f"/home/overlord/data/도시활력지수.csv"
+
+# SSH 키 로드
+key = paramiko.RSAKey.from_private_key_file(pem_file)
+
+# SSH 연결
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    ssh.connect(
+        hostname=hostname,
+        username=username,
+        pkey=key
+    )
+
+    # SFTP 시작
+    sftp = ssh.open_sftp()
+
+    # 업로드
+    sftp.put(local_file, remote_file)
+
+    print("파일 전송 완료")
+
+except Exception as e:
+    print(f"오류: {e}")
+
+finally:
+    sftp.close()
+    ssh.close()
